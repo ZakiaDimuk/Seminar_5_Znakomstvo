dd7b1a4e2a3848f2be772051c889550c4ce3f8db
ZakiaDim <dimukasheva97@mail.ru>
2023-03-28 19:40:41 +0500

    Z1

--- Zadanie_1/Program.cs
+++ Zadanie_1/Program.cs
@@ -2,8 +2,8 @@
 // которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] array = GetRandomArray(3, 100, 999);
-Console.WriteLine($"[{String.Join(",",array)}]");
+int[] array = GetRandomArray(7, 100, 999);
+Console.WriteLine($"[{String.Join(", ",array)}]");
 
 int count = 0;
 
@@ -11,7 +11,7 @@ for (int i = 0; i < array.Length; i++)
 {
 if (array[i] % 2 == 0) count++;
 }
-Console.WriteLine($"Четные = {count}");
+Console.WriteLine($"Количество чётных = {count}");
 
 // -------------------------------Общий метод-------------------------------------------
 int[] GetRandomArray(int size, int minValue, int maxValue)
